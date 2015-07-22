52e6493a480713ab15df2d6d45d68fb37cc09d77
guruahn <guruahn@gmail.com>
2015-07-22 19:47:38 +0900

    init

---
+++ app/components/main.jsx
@@ -0,0 +1,125 @@
+/** In this file, we create a React component which incorporates components provided by material-ui */
+
+let React = require('react');
+let mui = require('material-ui');
+let RaisedButton = mui.RaisedButton;
+let Dialog = mui.Dialog;
+let AppBar = mui.AppBar;
+let LeftNav = mui.LeftNav;
+let MenuItem = mui.MenuItem;
+let IconButton = mui.IconButton;
+let FlatButton = mui.FlatButton;
+let Paper = mui.Paper;
+
+let ThemeManager = new mui.Styles.ThemeManager();
+let Colors = mui.Styles.Colors;
+
+let Main = React.createClass({
+
+  childContextTypes: {
+    muiTheme: React.PropTypes.object
+  },
+
+  getChildContext() {
+    return {
+      muiTheme: ThemeManager.getCurrentTheme()
+    };
+  },
+
+  componentWillMount() {
+    ThemeManager.setPalette({
+      accent1Color: Colors.deepOrange500
+    });
+  },
+
+    render() {
+
+        let containerStyle = {
+          textAlign: 'center',
+          paddingTop: '0',
+          width: '750px',
+          margin: '0 auto',
+          backgroundColor: '#ddd'
+        };
+
+        let standardActions = [
+          { text: 'Okay' }
+        ];
+
+        return (
+          <div style={containerStyle}>
+            <Dialog
+              title="Super Secret Password"
+              actions={standardActions}
+              ref="superSecretPasswordDialog">
+              1-2-3-4-5
+            </Dialog>
+            <TopBar onLeftIconButtonTouchTap={this._onLeftIconButtonTouchTap} onRightIconButtonTouchTap={this._onRightIconButtonTouchTap}/>
+
+            <h1>material-ui</h1>
+            <h2>example project</h2>
+
+            <RaisedButton label="Super Secret Password" primary={true}  />
+            <Paper zDepth={1}>
+                <p>zDepth=1</p>
+            </Paper>
+            //Hideable Left Nav
+            <LeftNav ref="leftNav" docked={false} menuItems={menuItems} />
+
+          </div>
+        );
+
+    },
+
+    _onLeftIconButtonTouchTap(){
+        this.refs.leftNav.toggle();
+    },
+    _onRightIconButtonTouchTap(){
+      this.refs.superSecretPasswordDialog.show();  
+    }
+
+});
+
+let TopBar = React.createClass({
+    _onLeftIconButtonTouchTap(){
+        this.props.onLeftIconButtonTouchTap();
+    },
+    _onRightIconButtonTouchTap() {
+        this.props.onRightIconButtonTouchTap();
+    },
+    render: function(){
+        
+        return(
+            <AppBar title="Some Day" iconClassNameRight="muidocs-icon-navigation-expand-more"  onLeftIconButtonTouchTap={this._onLeftIconButtonTouchTap} onRightIconButtonTouchTap={this._onRightIconButtonTouchTap} />
+            
+        ); 
+        
+    }
+    
+});
+
+let menuItems = [
+  { route: 'get-started', text: 'Get Started' },
+  { route: 'customization', text: 'Customization' },
+  { route: 'components', text: 'Components' },
+  { type: MenuItem.Types.SUBHEADER, text: 'Resources' },
+  { 
+     type: MenuItem.Types.LINK, 
+     payload: 'https://github.com/callemall/material-ui', 
+     text: 'GitHub' 
+  },
+  { 
+     text: 'Disabled', 
+     disabled: true 
+  },
+  { 
+     type: MenuItem.Types.LINK, 
+     payload: 'https://www.google.com', 
+     text: 'Disabled Link', 
+     disabled: true 
+  },
+];
+
+
+
+module.exports = Main;
